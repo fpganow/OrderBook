06e8be15a2abee1fd41417ad3e4439a4b20c3de4
Fpga John <john@fpganow.com>
2021-03-21 17:53:12 -0400

    Ready

--- vivado/src/hdl/test/bats_parser_tb.v
+++ vivado/src/hdl/test/bats_parser_tb.v
@@ -46,8 +46,13 @@ module bats_parser_tb;
     reg[7:0] bats_data;
     wire [15:0] orderbook_command;
     wire orderbook_command_valid;
+    wire [31:0] cancelled_quantity;
+    wire [31:0] executed_quantity;
+    wire [63:0] price;
     wire [63:0] symbol;
-
+    wire [31:0] quantity;
+    wire [63:0] order_id;
+    wire [7:0] side;
 
     NiFpgaIPWrapper_bats_parser_ip UUT (
 		.reset(0),
@@ -55,27 +60,24 @@ module bats_parser_tb;
         .enable_out(),
         .enable_clr(0),
         .ctrlind_00_Ready_for_OrderBook_Command(1),
-        .ctrlind_01_OrderBook_Command_Valid(orderbook_command_valid),      // out std_logic_vector(0 downto 0);
-        .ctrlind_02_Cancelled_Quantity_U32(),       // out std_logic_vector(31 downto 0);
-        .ctrlind_03_Executed_Quantity_U32(),        // out std_logic_vector(31 downto 0);
-        .ctrlind_04_Price_U64(),                    // out std_logic_vector(63 downto 0);
-        .ctrlind_05_Symbol_U64(symbol),                   // out std_logic_vector(63 downto 0);
-        .ctrlind_06_Quantity_U32(),                 // out std_logic_vector(31 downto 0);
-        .ctrlind_07_Order_Id_U64(),                 // out std_logic_vector(63 downto 0);
-        .ctrlind_08_Side_U8(),                      // out std_logic_vector(7 downto 0);
-        .ctrlind_09_OrderBook_Command(orderbook_command),            // out std_logic_vector(15 downto 0);
-        .ctrlind_10_data_valid(data_valid_in),  // in std_logic_vector(0 downto 0);
-        .ctrlind_11_data(bats_data),              // in std_logic_vector(7 downto 0);
-        .ctrlind_12_reset(0),                   // in std_logic_vector(0 downto 0);
-        .ctrlind_13_Ready_for_Udp_Input(), // out std_logic_vector(0 downto 0);
+        .ctrlind_01_OrderBook_Command_Valid(orderbook_command_valid), // out std_logic_vector(0 downto 0);
+        .ctrlind_02_Cancelled_Quantity_U32(cancelled_quantity),       // out std_logic_vector(31 downto 0);
+        .ctrlind_03_Executed_Quantity_U32(executed_quantity),         // out std_logic_vector(31 downto 0);
+        .ctrlind_04_Price_U64(price),                                 // out std_logic_vector(63 downto 0);
+        .ctrlind_05_Symbol_U64(symbol),                               // out std_logic_vector(63 downto 0);
+        .ctrlind_06_Quantity_U32(quantity),                           // out std_logic_vector(31 downto 0);
+        .ctrlind_07_Order_Id_U64(order_id),                           // out std_logic_vector(63 downto 0);
+        .ctrlind_08_Side_U8(side),                                    // out std_logic_vector(7 downto 0);
+        .ctrlind_09_OrderBook_Command(orderbook_command),             // out std_logic_vector(15 downto 0);
+        .ctrlind_10_data_valid(data_valid_in),                        // in std_logic_vector(0 downto 0);
+        .ctrlind_11_data(bats_data),                                  // in std_logic_vector(7 downto 0);
+        .ctrlind_12_reset(0),                                         // in std_logic_vector(0 downto 0);
+        .ctrlind_13_Ready_for_Udp_Input(),                            // out std_logic_vector(0 downto 0);
         .Clk40Derived2x1I0MHz(clk)
     );
 
-
     integer fptr;
     integer scan_faults;
-    
-    //reg[7:0] bats_data [0:7];
 
     initial
     begin
@@ -90,16 +92,10 @@ module bats_parser_tb;
             scan_faults = $fread(bats_data, fptr);
             data2_in = bats_data;
             data_valid_in = 1'b1;
-            //data2_in = 8'b11101001;
             #period;           
         end
         $fclose(fptr); // Close file before finish
 
         $finish;
     end
-
-//    always @(posedge clk)
-//    begin
-//        data_valid_in = 1'b1;
-//    end
 endmodule
