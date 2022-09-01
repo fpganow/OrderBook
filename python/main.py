e45d251662bf8fd7c92df568fcf31edff9a693b6
Fpga John <john@fpganow.com>
2022-09-01 12:29:15 -0400

    Adding some python code

---
+++ python/main.py
@@ -0,0 +1,27 @@
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Based on the following document
+#  - filename: US_EQUITIES_OPTIONS_MULTICAST_PITCH_SPECIFICATION.pdf
+#  - title: US Equities/Options Multicast Depth of Book (PITCH) Specification
+#  - version: 2.41.9
+#  - date: February 1st, 2021
+
+
+# Generate a lot of Market Data messages and:
+#  - send over an actual UDP connection
+#  - save to a file
+#  - Use a specific list of tickers
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
